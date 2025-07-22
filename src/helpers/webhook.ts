1d785b3d2481738ee8dad6ba7fba4619a9735d7f
Jeremio <jcotiniaux@gmail.com>
2025-07-22 18:04:36 +0200

    update deps

--- src/helpers/webhook.ts
+++ src/helpers/webhook.ts
@@ -1,11 +1,9 @@
 async function post(url: string, body: any) {
   return fetch(url, {
     method: 'POST',
-    mode: 'no-cors',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify(body),
   })
